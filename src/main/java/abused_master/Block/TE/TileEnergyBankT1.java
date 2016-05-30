57d8fe4c890f50c733f54646d0f2c9adc206c1c4
abused_master <techperson71@gmail.com>
2016-05-30 08:35:10 -0700

    Changed Energy Methods

--- src/main/java/abused_master/Block/TE/TileEnergyBankT1.java
+++ src/main/java/abused_master/Block/TE/TileEnergyBankT1.java
@@ -17,126 +17,49 @@ import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.eventhandler.Event;
 
-public class TileEnergyBankT1 extends TileEntity implements IEnergyStorage {
+public class TileEnergyBankT1 extends TileEntity implements IEnergyProvider, IEnergyReceiver {
 
-	public int energy;
-	public int capacity;
-	public int maxReceive;
-	public int maxExtract;
 
-	public TileEnergyBankT1(int capacity) {
+	protected EnergyStorage storage = new EnergyStorage(1000000);
 
-		this(capacity, capacity, capacity);
-	}
-
-	public TileEnergyBankT1(int capacity, int maxTransfer) {
-
-		this(capacity, maxTransfer, maxTransfer);
-	}
-
-	public TileEnergyBankT1(int capacity, int maxReceive, int maxExtract) {
-
-		this.capacity = 1000000;
-		this.maxReceive = 10000;
-		this.maxExtract = 10000;
-	}
-
-	public TileEnergyBankT1() {
-		// TODO Auto-generated constructor stub
-	}
-
+	@Override
 	public void readFromNBT(NBTTagCompound nbt) {
 
-		this.energy = nbt.getInteger("Energy");
-
-		if (energy > capacity) {
-			energy = capacity;
-		}
-		return;
+		super.readFromNBT(nbt);
+		storage.readFromNBT(nbt);
 	}
 
+	@Override
 	public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
 
-		if (energy < 0) {
-			energy = 0;
-		}
-		nbt.setInteger("Energy", energy);
+		super.writeToNBT(nbt);
+		storage.writeToNBT(nbt);
 		return nbt;
 	}
-
-	public TileEnergyBankT1 setCapacity(int capacity) {
-
-		this.capacity = capacity;
-
-		if (energy > capacity) {
-			energy = capacity;
-		}
-		return this;
-	}
-
-	public TileEnergyBankT1 setMaxTransfer(int maxTransfer) {
-
-		setMaxReceive(maxTransfer);
-		setMaxExtract(maxTransfer);
-		return this;
-	}
-
-	public TileEnergyBankT1 setMaxReceive(int maxReceive) {
-
-		this.maxReceive = maxReceive;
-		return this;
-	}
-
-	public TileEnergyBankT1 setMaxExtract(int maxExtract) {
-
-		this.maxExtract = maxExtract;
-		return this;
-	}
-
-	public int getMaxReceive() {
-
-		return maxReceive;
-	}
-
-	public int getMaxExtract() {
-
-		return maxExtract;
-	}
-
-	/* IEnergyStorage */
+	
 	@Override
-	public int receiveEnergy(int maxReceive, boolean simulate) {
-
-		int energyReceived = Math.min(capacity - energy, Math.min(this.maxReceive, maxReceive));
-
-		if (!simulate) {
-			energy += energyReceived;
-		}
-		return energyReceived;
+	public int getEnergyStored(EnumFacing from) {
+		return 0;
 	}
 
 	@Override
-	public int extractEnergy(int maxExtract, boolean simulate) {
-
-		int energyExtracted = Math.min(energy, Math.min(this.maxExtract, maxExtract));
-
-		if (!simulate) {
-			energy -= energyExtracted;
-		}
-		return energyExtracted;
+	public int getMaxEnergyStored(EnumFacing from) {
+		return 0;
 	}
 
 	@Override
-	public int getEnergyStored() {
-
-		return energy;
+	public boolean canConnectEnergy(EnumFacing from) {
+		return false;
 	}
 
 	@Override
-	public int getMaxEnergyStored() {
-
-		return capacity;
+	public int receiveEnergy(EnumFacing from, int maxReceive, boolean simulate) {
+		return 0;
 	}
 
+	@Override
+	public int extractEnergy(EnumFacing from, int maxExtract, boolean simulate) {
+		return 0;
+	}
 	
 }
